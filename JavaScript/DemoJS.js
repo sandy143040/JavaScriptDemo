62ce89ba1f0b6dcd648f4a931f5759f1885c96dc
Sandeep Kumar Singh <109274261+sandy143040@users.noreply.github.com>
2023-08-09 18:46:09 +0530

    Add files via upload

---
+++ JavaScript/DemoJS.js
@@ -0,0 +1,48 @@
+//console.log("Sandeep")
+//console.log("Sandeep")
+/*console.log("Sandeep")
+console.log("Singh") */
+
+//let keyword is used to declare a varible
+let name = "Sandeep";
+console.log(name);
+
+//var is leble that references a value 
+var names="Sameer";
+console.log(names);
+
+//const is read-only value
+const int=1;
+console.log(int);
+
+//DataType-
+let num=4;
+console.log(typeof(num));
+number="Sandeep";
+console.log(typeof(number));
+number=true;
+console.log(typeof(number));
+number=544.434;
+console.log(typeof(number));
+
+//Array
+let array=new Array();
+let arrays=new Array(10);
+let arr=new Array(1,2,3,4,5,6,7,8,9,10);
+let fruits=["Bannana","Apple"];
+console.log(fruits[0]);
+fruits[2]="Kiwi";
+console.log(fruits);
+console.log(fruits.length);
+fruits.push("Mango");
+console.log(fruits);
+fruits.unshift("Orange");
+console.log(fruits);
+fruits.pop();
+console.log(fruits);
+fruits.shift();
+console.log(fruits);
+fruits.indexOf("Apple");
+console.log(fruits.indexOf("Apple"));
+console.log(Array.isArray(fruits));
+
